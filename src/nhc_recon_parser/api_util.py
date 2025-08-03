a33232b03449130f4b9ee1450be55fd2a78d697c
Zachary Davis <zachary.davis0502@gmail.com>
2025-08-03 16:50:15 -0700

    added function to create a collection, STAC item sounding system parsing error catching, create collection boolean

--- src/nhc_recon_parser/api_util.py
+++ src/nhc_recon_parser/api_util.py
@@ -4,7 +4,31 @@ import json
 from pystac import Collection, Extent, SpatialExtent, TemporalExtent
 import requests
 
+CREATE_COLLECTION = True  # Set to False if you want to skip collection creation if it doesnt exist
 # load_dotenv()  # Load environment variables from .env file
+
+def create_dropsonde_collection(collection_id: str, api_base_url: str):
+    collection_description = "Collection of dropsonde observations from NHC Recon flights"
+    collection_title = "Dropsonde Observations"
+
+    # Define initial extent (will be updated by PgSTAC if enabled)
+    initial_extent = Extent(
+        spatial=SpatialExtent(bboxes=[[-180.0, -90.0, 180.0, 90.0]]), # Global Extent (WGS84)
+        temporal=TemporalExtent(intervals=[None, None]) # Dont define a time range
+    )
+
+    stac_collection = Collection(
+        id=collection_id,
+        description=collection_description,
+        title=collection_title,
+        extent=initial_extent,
+        license="CC-BY-4.0"
+    )
+    session = requests.Session()
+    res = session.post(f'{api_base_url}/collections',json=stac_collection.to_dict())
+    res.raise_for_status()
+    print(f"Collection '{collection_id}' created successfully.")
+    return res.reason
 
 # conn_str = f"postgresql://{os.getenv('PGUSER')}:{os.getenv('PGPASSWORD')}@{os.getenv('PGHOST')}:{os.getenv('PGPORT')}/{os.getenv('PGDATABASE')}"
 def add_item_to_collection(stac_item: "pystac.Item", collection_id: str, api_base_url: str):
@@ -16,7 +40,10 @@ def add_item_to_collection(stac_item: "pystac.Item", collection_id: str, api_bas
         requests.exceptions.HTTPError: STAC item post request failed
     """    
     session = requests.Session()
-    if collection_existance_check(collection_id, api_base_url):
+    col_exists = collection_existance_check(collection_id, api_base_url)
+    if col_exists or CREATE_COLLECTION:
+        if not col_exists:
+            create_dropsonde_collection(collection_id, api_base_url)
         res = session.post(f'{api_base_url}/collections/{collection_id}/items',json=stac_item.to_dict())
         res.raise_for_status()
         return res.reason
@@ -30,4 +57,5 @@ def collection_existance_check(collection_id: str, api_base_url: str):
     if res.status_code == 200:
         return True
     
+    print('Collection with id "%s" does not exist!' % collection_id)
     return False
